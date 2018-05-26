e56680670d10600e86d370c5dab17275526597cf
Jure Zajc <Jure.Zajc.95@Gmail.Com>
2018-05-26 14:32:56 +0200

    Update UV_test.java
    With this change i got a 6

--- UV_test.java
+++ UV_test.java
@@ -2,16 +2,22 @@ import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.ImageFilter;
 import java.io.IOException;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import SteganographicImage.SteganographicImageException;
 
@@ -25,10 +31,12 @@ public class UV_test extends JFrame {
 	private JTextField locationWebPhoto;
 	private JTextField locationWebPhoto2;
 	private JTextField locationLocal;
-	private JTextField text;
+	private JTextArea textArea;
+	String selectedPicture;
 //	private JTextField sizePict;
-//	private JTextField attachment;
-
+	private JButton attachment1;
+	private JButton attachment;
+	String attach;
 	private JPanel contentPane;
 
 	/**
@@ -51,7 +59,7 @@ public class UV_test extends JFrame {
 	 * Create the frame.
 	 */
 	public UV_test() {
-		super("UV_naloga2");
+		super("UV_assignment2");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1200, 350);
 		contentPane = new JPanel();
@@ -65,34 +73,81 @@ public class UV_test extends JFrame {
 		locationWebPhoto.setColumns(120); // stevilo stolpcev
 		locationWebPhoto.setToolTipText("Enter direct URL to image");
 		contentPane.add(locationWebPhoto); // dodajanje na plosco
-		JLabel getPhotoWeb1 = new JLabel("Get another photo from web"); // definicija oznake
-		getPhotoWeb1.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
-		contentPane.add(getPhotoWeb1); // dodajanje na plosco
-		locationWebPhoto2 = new JTextField(); // prazno besedilno polje
-		locationWebPhoto2.setColumns(120); // stevilo stolpcev
-		locationWebPhoto2.setToolTipText("Enter direct URL to image");
-		contentPane.add(locationWebPhoto2); // dodajanje na plosco
-		JLabel lblDrzavaISO = new JLabel("Choose a photo localy"); // definicija oznake
+//		JLabel getPhotoWeb1 = new JLabel("Get another photo from web"); // definicija oznake
+//		getPhotoWeb1.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
+//		contentPane.add(getPhotoWeb1); // dodajanje na plosco
+//		locationWebPhoto2 = new JTextField(); // prazno besedilno polje
+//		locationWebPhoto2.setColumns(120); // stevilo stolpcev
+//		locationWebPhoto2.setToolTipText("Enter direct URL to image");
+//		contentPane.add(locationWebPhoto2); // dodajanje na plosco
+		JLabel lblDrzavaISO = new JLabel("Choose a photo locally"); // definicija oznake
 		lblDrzavaISO.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
 		contentPane.add(lblDrzavaISO); // dodajanje na plosco	
 		locationLocal = new JTextField(); // prazno besedilno polje
 		locationLocal.setColumns(80); // stevilo stolpcev
-		locationLocal.setToolTipText("Enter absolute path to image");
+		locationLocal.setToolTipText("Enter absolute path to image or use FileChoser button at the bottom");
 		contentPane.add(locationLocal); // dodajanje na plosco
 		JLabel lblOznaka2 = new JLabel("Edit text"); // definicija oznake
 		lblOznaka2.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
 		contentPane.add(lblOznaka2); // dodajanje na plosco		
-		text = new JTextField(); // prazno besedilno polje
-		text.setColumns(120); // stevilo stolpcev
-		text.setToolTipText("Enter text that you want to encrypt");
-		contentPane.add(text); // dodajanje na plosco
-//		JLabel lblOznaka212 = new JLabel("Add attachments"); // definicija oznake
-//		lblOznaka212.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
-//		contentPane.add(lblOznaka212); // dodajanje na plosco		
-//		attachment = new JTextField(); // prazno besedilno polje
-//		attachment.setColumns(120); // stevilo stolpcev
-//		attachment.setToolTipText("Add attachments");
-//		contentPane.add(attachment); // dodajanje na plosco
+		textArea = new JTextArea(); // prazno besedilno polje
+		textArea.setColumns(120); // stevilo stolpcev
+		textArea.setToolTipText("Enter text that you want to encrypt");
+		contentPane.add(textArea); // dodajanje na plosco
+		
+	    //int returnVal = chooser.showOpenDialog();
+//	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+//	       System.out.println("You chose to open this file: " +
+//	            chooser.getSelectedFile().getName());
+//	    }
+		JLabel lblOznaka212 = new JLabel("Add attachments"); // definicija oznake
+		lblOznaka212.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
+		contentPane.add(lblOznaka212); // dodajanje na plosco		
+		attachment1 = new JButton("Add attachments"); // prazno besedilno polje
+		final JFileChooser test = new JFileChooser();
+		attachment1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				test.setCurrentDirectory(new java.io.File("user.home"));
+				test.setDialogTitle("Choose a txt file you wanna encrypt");
+				FileNameExtensionFilter filterTextt = new FileNameExtensionFilter(".Text", "txt");
+				test.addChoosableFileFilter(filterTextt);
+				if (test.showOpenDialog(attachment1)== JFileChooser.APPROVE_OPTION) {
+					//String attach = test.getSelectedFile().getAbsolutePath();
+					JOptionPane.showMessageDialog(contentPane, test.getSelectedFile().getAbsolutePath(), "Selected file", JOptionPane.DEFAULT_OPTION);
+					
+					
+				}
+				String testni = test.getSelectedFile().getName();
+				test.setDialogTitle(testni);
+				attach = test.getSelectedFile().getAbsolutePath();
+			}
+		});
+		/*attachment.setColumns(120); // stevilo stolpcev
+		attachment.setToolTipText("Add attachments");*/
+		contentPane.add(attachment1); // dodajanje na plosco
+		final JButton openFileChooser = new JButton("Open JFileChooser for local picture");
+		  final JFileChooser fc = new JFileChooser();
+	        openFileChooser.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+
+	                fc.setCurrentDirectory(new java.io.File("user.home"));
+	                fc.setDialogTitle("Choose a picture you wanna encrypt");
+	                fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+	                FileNameExtensionFilter filter = new FileNameExtensionFilter(".Image", "jpg", "gif", "png");
+	                fc.addChoosableFileFilter(filter);
+//	                FileNameExtensionFilter filterText = new FileNameExtensionFilter(".Text", "txt");
+//	                fc.addChoosableFileFilter(filterText);
+	                if (fc.showOpenDialog(openFileChooser) == JFileChooser.APPROVE_OPTION) {
+	                	selectedPicture = fc.getSelectedFile().getAbsolutePath();
+	                    JOptionPane.showMessageDialog(contentPane, fc.getSelectedFile().getAbsolutePath(), "Selected picture", JOptionPane.DEFAULT_OPTION);
+	                }
+	                locationLocal.setText(selectedPicture);
+	            }
+	        });
+	    contentPane.add(openFileChooser);    
 //		JLabel lblOznaka21 = new JLabel("Check remaining space"); // definicija oznake
 //		lblOznaka21.setHorizontalAlignment(SwingConstants.CENTER); // postavitev na sredino stolpca
 //		contentPane.add(lblOznaka21); // dodajanje na plosco		
@@ -110,32 +165,50 @@ public class UV_test extends JFrame {
 				SteganographicImage stegon = null;
 				SteganographicImage stegon2 = null;
 				if(locationWebPhoto.getText().isEmpty()) {
-					try {
-						stegon =
-							     SteganographicImage.loadFromFile(locationLocal.getText());
+					if (!locationLocal.getText().isEmpty()) {
+						try {
+							stegon =
+								     SteganographicImage.loadFromFile(locationLocal.getText());
 
-						
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+							
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					}
+					else {
+						try {
+							stegon =
+								     SteganographicImage.loadFromFile(selectedPicture);
+							
+
+							
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+					
 					try {
-						stegon.setMessage(text.getText());
+						stegon.setMessage(textArea.getText());
 					} catch (SteganographicImage.SteganographicImageException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-//					try {
-//						try {
-//							stegon.addAttachment(attachment.getText());
-//						} catch (SteganographicImage.SteganographicImageException e1) {
-//							// TODO Auto-generated catch block
-//							e1.printStackTrace();
-//						}
-//					} catch (IOException e2) {
-//						// TODO Auto-generated catch block
-//						e2.printStackTrace();
-//					}
+					if (!(attach.length() == 0)) {
+						try {
+							try {
+								stegon.addAttachment(attach);
+							} catch (SteganographicImage.SteganographicImageException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						} catch (IOException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+					}
+					
 					try {
 						Random rand = new Random();
 						int  n = rand.nextInt(500) + 1;
@@ -156,22 +229,24 @@ public class UV_test extends JFrame {
 						e1.printStackTrace();
 					}
 					try {
-						stegon.setMessage(text.getText());
+						stegon.setMessage(textArea.getText());
 					} catch (SteganographicImage.SteganographicImageException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-//					try {
-//						try {
-//							stegon.addAttachment(attachment.getText());
-//						} catch (SteganographicImage.SteganographicImageException e1) {
-//							// TODO Auto-generated catch block
-//							e1.printStackTrace();
-//						}
-//					} catch (IOException e2) {
-//						// TODO Auto-generated catch block
-//						e2.printStackTrace();
-//					}
+					if (!(attach.length() == 0)) {
+						try {
+							try {
+								stegon.addAttachment(attach);
+							} catch (SteganographicImage.SteganographicImageException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						} catch (IOException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+					}
 					try {
 						Random rand = new Random();
 						int  n = rand.nextInt(500) + 1;
@@ -181,23 +256,25 @@ public class UV_test extends JFrame {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-					if(!locationWebPhoto2.getText().isEmpty()) {
-						
-						try {
-							stegon2 = SteganographicImage.loadFromUrl(locationWebPhoto2.getText());
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						try {
-							stegon.setMessage(text.getText());
-						} catch (SteganographicImage.SteganographicImageException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
+					dispose();
+//					if(!locationWebPhoto2.getText().isEmpty()) {
+//						
+//						try {
+//							stegon2 = SteganographicImage.loadFromUrl(locationWebPhoto2.getText());
+//						} catch (IOException e1) {
+//							// TODO Auto-generated catch block
+//							e1.printStackTrace();
+//						}
+//						try {
+//							stegon.setMessage(textArea.getText());
+//						} catch (SteganographicImage.SteganographicImageException e1) {
+//							// TODO Auto-generated catch block
+//							e1.printStackTrace();
+//						}
+//						if (!(attach.length() == 0)) {
 //						try {
 //							try {
-//								stegon.addAttachment(attachment.getText());
+//								stegon.addAttachment(attach);
 //							} catch (SteganographicImage.SteganographicImageException e1) {
 //								// TODO Auto-generated catch block
 //								e1.printStackTrace();
@@ -206,17 +283,18 @@ public class UV_test extends JFrame {
 //							// TODO Auto-generated catch block
 //							e2.printStackTrace();
 //						}
-						try {
-							Random rand = new Random();
-							int  n = rand.nextInt(500) + 1;
-							stegon2.saveToFile("Example_ecnodedWebVersion2x"+n+".png");
-							System.out.println("Image saved here: C:\\Users\\JURE\\workspace\\UV_2\\example_encodedWebVersion2x"+n+".png");
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					}
-					dispose();
+//					}
+//						try {
+//							Random rand = new Random();
+//							int  n = rand.nextInt(500) + 1;
+//							stegon2.saveToFile("Example_ecnodedWebVersion2x"+n+".png");
+//							System.out.println("Image saved here: C:\\Users\\JURE\\workspace\\UV_2\\example_encodedWebVersion2x"+n+".png");
+//						} catch (IOException e1) {
+//							// TODO Auto-generated catch block
+//							e1.printStackTrace();
+//						}
+//					}
+//					dispose();
 				}	
 //				if(!(sizePict.getText().isEmpty()) && locationWebPhoto.getText().isEmpty() &&locationWebPhoto2.getText().isEmpty()) {
 //					try {
@@ -264,31 +342,34 @@ public class UV_test extends JFrame {
 				try {
 					stegon = SteganographicImage.loadFromFile(locationLocal.getText());
 	    			System.out.printf("Hidden message is: "+stegon.getMessage() +".\n");
-//	    			String[] attachments = stegon.listAttachments();
-//	    			System.out.println("Attachments:");
-//	    			for (int i = 0; i < attachments.length; i++) {
-//	    				System.out.println(attachments[i]);
-//	    			    try {
-//	    			    	try {
-//								stegon.saveAttachment(attachments[i]);
-//							} catch (SteganographicImage.SteganographicImageException e1) {
-//								// TODO Auto-generated catch block
-//								e1.printStackTrace();
-//							}
-//						} catch (IOException e1) {
-//							// TODO Auto-generated catch block
-//							e1.printStackTrace();
-//						}
-//	    			  }
+	    			String[] attachments = stegon.listAttachments();
+	    			System.out.println("Attachments:");
+	    			for (int i = 0; i < attachments.length; i++) {
+	    				System.out.println(attachments[i]);
+	    			    try {
+	    			    	try {
+								stegon.saveAttachment(attachments[i]);
+							} catch (SteganographicImage.SteganographicImageException e1) {
+								// TODO Auto-generated catch block
+								JOptionPane.showMessageDialog(contentPane, "Please select a file", "Empty file", JOptionPane.ERROR_MESSAGE);
+								//e1.printStackTrace();
+							}
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							JOptionPane.showMessageDialog(contentPane, "Please select a file", "Empty file", JOptionPane.ERROR_MESSAGE);
+							//e1.printStackTrace();
+						}
+	    			  }
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					JOptionPane.showMessageDialog(contentPane, "Please select a file", "Empty file", JOptionPane.ERROR_MESSAGE);
+					//e1.printStackTrace();
 				}
 				
 			}
 		});
 		buttons.add(btnActions);
-		btnStorage = new JButton("Check remaing storage");
+		btnStorage = new JButton("Check remaining storage");
 		btnStorage.addActionListener(new ActionListener() {
 			SteganographicImage stegon = null;
 			
@@ -296,11 +377,15 @@ public class UV_test extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					stegon = SteganographicImage.loadFromFile(locationLocal.getText());
+					System.out.println("Remaining storage is: "+stegon.getUsedStorage()+"bytes.");
 				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(contentPane, "Please select a file", "Empty file", JOptionPane.ERROR_MESSAGE);
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					
+					//System.out.println("Please select a file");
+					//e1.printStackTrace();
 				}
-				 System.out.println("Remaining storage is: "+stegon.getUsedStorage()+"%.");
+				 
 				
 			}
 		});
